0c1d2312a59ba050f058f1adf59e6252fbec7a50
Alina <alina.bianca05@yahoo.com>
2024-03-14 13:57:22 +0200

    alina

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react_deploy_todo_list_gh/",
+  base: "/todo_list_react_vite/",
   plugins: [react()],
 })
